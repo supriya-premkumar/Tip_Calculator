328a64f9a8717c80682de81404f502ecce89a9cc
supriya-premkumar <supriya@fastmail.com>
2016-12-18 02:50:50 -0800

    Added html page

---
+++ TipQuick/TipCalculator.php
@@ -0,0 +1,27 @@
+<form  name="form" action="" method="post">
+   <table border="0">
+     <tr>
+       <td>Bill SubTotal: $ </td>
+       <td><input type="text" name="bill"></td>
+     </tr><tr>
+       <td>Tip Percentage:</td>
+     </tr><tr>
+       <td><input type="radio" name="percent" value="10">10%</td>
+       <td><input type="radio" name="percent" value="15">15%</td>
+       <td><input type="radio" name="percent" value="20">20%</td>
+     </tr><tr>
+       <td><input type="radio" name="percent" value="custom">custom:</td>
+       <td><input type ="text" name="custom">%</td>
+     </tr><tr>
+     </tr><tr>
+     </tr><tr>
+       <td>split:</td>
+       <td><input type ="text" name = "split"> person(s)</td>
+     </tr><tr>
+     </tr><tr>
+     </tr><tr>
+       <td></td>
+       <td><input type="submit" name="submit" value="Submit"></td>
+</tr>
+</table>
+</form>
