dcc1703fb7171a3a249cd39a2dbbfcae099f08c7
supriya-premkumar <supriya@fastmail.com>
2016-12-18 19:04:31 -0800

    Implemented custom tip and split functionalities

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ Time spent: **x** hours spent in total
 ## User Stories
 
 The following **required** functionality is complete:
-* [ ] User can enter a bill amount, choose a tip percentage, and submit the form to see the tip and total values.
+* [x] User can enter a bill amount, choose a tip percentage, and submit the form to see the tip and total values.
 * [ ] Tip percentage choices use a PHP loop to output three radio buttons.
-* [ ] PHP code sets reasonable default values for the form.
+* [x] PHP code sets reasonable default values for the form.
 * [ ] PHP code confirms the presence and correct format of submitted values.
 * [ ] Page indicates any form errors which need to be fixed.
 * [ ] Submitted form values are retained when errors or results are shown.
 
 The following **optional** features are implemented:
-* [ ] Add support for custom tip percentage
-* [ ] Add support for splitting the tip and total
+* [x] Add support for custom tip percentage
+* [x] Add support for splitting the tip and total
 
 The following **additional** features are implemented:
 
--- TipQuick/TipCalculator.php
+++ TipQuick/TipCalculator.php
@@ -3,39 +3,41 @@
   <head>
     <meta charset="utf-8">
     <title> TipQuick </title>
-    <style>
-    @import "http://fonts.googleapis.com/css?family=Droid+Serif";
-    /* Above line is used for online google font */
-    div.container {
-    width:960px;
-    height:610px;
-    margin:50px auto;
-    font-family:'Droid Serif',serif
-    }
-    div.main {
-    width:308px;
-    float:left;
-    border-radius:5px;
-    border:2px solid #990;
-    padding:0 50px 20px
-    }
-
-    h1{
-      color: #b92b27;
-      width:960px;
+    <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+    <div class="container">
+      <style>
+      @import "http://fonts.googleapis.com/css?family=Droid+Serif";
+      /* Above line is used for online google font */
+      div.container {
+      width:500px;
+      height:250px;
       margin:50px auto;
       font-family:'Droid Serif',serif
-      /*text-align:center;*/
-    }
-    td{
-      color: #2316E6;
-    }
-    </style>
+      }
+      div.main {
+      width:308px;
+      float:left;
+      border-radius:5px;
+      border:2px solid #990;
+      padding:0 50px 20px
+      }
+
+      h1{
+        color: #b92b27;
+        width:960px;
+        margin:50px auto;
+        font-family:'Droid Serif',serif
+        /*text-align:center;*/
+      }
+      td{
+        color: #2316E6;
+      }
+      </style>
+    <h1>Welcome to TipQuick!</h1>
   </head>
   <body>
-      <h1>Welcome to TipQuick!</h1>
-      <div class="container">
-      <div class="main">
+    <div class="container">
+    <div class="main">
     <form  name="form" action="" method="post">
        <table border="0">
          <tr>
@@ -44,12 +46,12 @@
          </tr><tr>
            <td>Tip Percentage:</td>
          </tr><tr>
-           <td><input type="radio" name="percent" value="10">10%</td>
-           <td><input type="radio" name="percent" value="15">15%</td>
-           <td><input type="radio" name="percent" value="20">20%</td>
+           <td width="94px" height="41px"><input type="radio" name="percent" value="10">10%</td>
+           <td width="94px" height="41px"><input type="radio" name="percent" value="15">15%</td>
+           <td width="94px" height="41px"><input type="radio" name="percent" value="20">20%</td>
          </tr><tr>
-           <td><input type="radio" name="percent" value="1">custom:</td>
-           <td><input type ="text" name="custom">%</td>
+           <td width="94px" height="41px"><input type="radio" name="customPercent" value="1">Custom Tip Percent: </td>
+           <td><input type ="text" name="custom"></td>
          </tr><tr>
          </tr><tr>
          </tr><tr>
@@ -67,51 +69,71 @@
 </div>
 
 
-
-
-
 <?php
+if(isset($_POST['bill'], $_POST['split'], $_POST['percent'])){
+         echo "Bill: $".$_POST['bill'];
+         if($_POST['percent']<0) // Making sure that percentage doesnt go into negatives
+         $_POST['percent'] = 0; //If it does, set it to 0
+         if($_POST['custom']<0) // Making sure that percentage doesnt go into negatives
+         $_POST['custom'] = 0; //If it does, set it to 0
+         echo "<br/>Tip %: $".$_POST['percent'];
+         if($_POST['split']<1) //Just making sure this number isnt less than 1
+         $_POST['split'] = 1; //If it is, set it to 1 which is the default
+         echo "<br/>People: $".$_POST['split'];
+         echo "<br/>Tip amount: $".$tip = number_format($_POST['bill'] * $_POST['percent'] / 100, 2);
+         echo "<br/>Total: $".$total = $tip+$_POST['bill'];
+         echo "<br/>Total per Person: $".$total/$_POST['split'];
+}
+
+if(isset($_POST['bill'], $_POST['split'], $_POST['customPercent'], $_POST['custom'])){
+  echo "Bill: $".$_POST['bill'];
+  if(isset($_POST['customPercent']) && isset($_POST['custom'])){
+    if($_POST['custom']<0) // Making sure that percentage doesnt go into negatives
+    $_POST['custom'] = 0; //If it does, set it to 0
+    echo "<br/>Tip %: $".$_POST['custom'];
+    if($_POST['split']<1) //Just making sure this number isnt less than 1
+    $_POST['split'] = 1; //If it is, set it to 1 which is the default
+    echo "<br/>People: $".$_POST['split'];
+    echo "<br/>Tip amount: $".$tip = number_format($_POST['bill'] * $_POST['custom'] / 100, 2);
+    echo "<br/>Total: $".$total = $tip+$_POST['bill'];
+    echo "<br/>Total per Person: $".$total/$_POST['split'];
+  }else{
+    echo "Please enter Custom Tip Value";
+  }
+
+}
+
+
 //Checks if all the required data has been acquired
-if(isset($_POST['bill'], $_POST['percent'], $_POST['split'])){
-	//Checks if the data is numeric as it should be
-	// if(is_numeric($_POST['bill']) && is_numeric($_POST['custom']) && is_numeric($_POST['split']) ){
-		echo "Bill: ".$_POST['bill'];
-
-    if (isset($_POST['percent']) && (isset($_POST['custom']))) {
-       if($_POST['custom']<0) // Making sure that percentage doesnt go into negatives
-  		 	$_POST['custom'] = 0; //If it does, set it to 0
-  		 echo "<br/>Tip %: ".$_POST['custom'];
-  		 if($_POST['split']<1) //Just making sure this number isnt less than 1
-  		 	$_POST['split'] = 1; //If it is, set it to 1 which is the default
-  		 echo "<br/>People: ".$_POST['split'];
-  		 echo "<br/>Tip amount: ".$tip = number_format($_POST['bill'] * $_POST['custom'] / 100, 2);
-  		 echo "<br/>Total: ".$total = $tip+$_POST['bill'];
-  		 echo "<br/>Total per Person: ".$total/$_POST['split'];
-    } else {
-    }
-    if(isset($_POST['percent'])){
-      //  if($_POST['percent']<0) // Making sure that percentage doesnt go into negatives
-      //  $_POST['custom'] = 0; //If it does, set it to 0
-       echo "<br/>Tip %: ".$_POST['percent'];
-       if($_POST['split']<1) //Just making sure this number isnt less than 1
-       $_POST['split'] = 1; //If it is, set it to 1 which is the default
-       echo "<br/>People: ".$_POST['split'];
-       echo "<br/>Tip amount: ".$tip = number_format($_POST['bill'] * $_POST['percent'] / 100, 2);
-       echo "<br/>Total: ".$total = $tip+$_POST['bill'];
-       echo "<br/>Total per Person: ".$total/$_POST['split'];
-    }
-
-		// if($_POST['custom']<0) // Making sure that percentage doesnt go into negatives
-		// 	$_POST['custom'] = 0; //If it does, set it to 0
-		// echo "<br/>Tip %: ".$_POST['custom'];
-		// if($_POST['split']<1) //Just making sure this number isnt less than 1
-		// 	$_POST['split'] = 1; //If it is, set it to 1 which is the default
-		// echo "<br/>People: ".$_POST['split'];
-		// echo "<br/>Tip amount: ".$tip = number_format($_POST['bill'] * $_POST['custom'] / 100, 2);
-		// echo "<br/>Total: ".$total = $tip+$_POST['bill'];
-		// echo "<br/>Total per Person: ".$total/$_POST['split'];
-	// }else echo "At least one of the values was not a number!";
-}else echo "Please fill all the values";
+// if(isset($_POST['bill'], $_POST['percent'], $_POST['split'])){
+// 	//Checks if the data is numeric as it should be
+// 	// if(is_numeric($_POST['bill']) && is_numeric($_POST['custom']) && is_numeric($_POST['split']) ){
+// 		echo "Bill: ".$_POST['bill'];
+//
+//     if (isset($_POST['percent']) && (isset($_POST['custom']))) {
+//        if($_POST['custom']<0) // Making sure that percentage doesnt go into negatives
+//   		 	$_POST['custom'] = 0; //If it does, set it to 0
+//   		 echo "<br/>Tip %: ".$_POST['custom'];
+//   		 if($_POST['split']<1) //Just making sure this number isnt less than 1
+//   		 	$_POST['split'] = 1; //If it is, set it to 1 which is the default
+//   		 echo "<br/>People: ".$_POST['split'];
+//   		 echo "<br/>Tip amount: ".$tip = number_format($_POST['bill'] * $_POST['custom'] / 100, 2);
+//   		 echo "<br/>Total: ".$total = $tip+$_POST['bill'];
+//   		 echo "<br/>Total per Person: ".$total/$_POST['split'];
+//     } else {
+//     }
+//     if(isset($_POST['percent'])){
+//       //  if($_POST['percent']<0) // Making sure that percentage doesnt go into negatives
+//       //  $_POST['custom'] = 0; //If it does, set it to 0
+//        echo "<br/>Tip %: ".$_POST['percent'];
+//        if($_POST['split']<1) //Just making sure this number isnt less than 1
+//        $_POST['split'] = 1; //If it is, set it to 1 which is the default
+//        echo "<br/>People: ".$_POST['split'];
+//        echo "<br/>Tip amount: ".$tip = number_format($_POST['bill'] * $_POST['percent'] / 100, 2);
+//        echo "<br/>Total: ".$total = $tip+$_POST['bill'];
+//        echo "<br/>Total per Person: ".$total/$_POST['split'];
+//     }
+// }else echo "Please fill all the values";
 ?>
 </body>
 </div>
